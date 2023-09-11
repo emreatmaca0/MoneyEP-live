1d3dbd816ac68b54d1104a795f71998c26b65103
Emre Atmaca <emreatmaca@mail.com.tr>
2023-09-11 18:31:05 +0300

    app/config path 2nd attempt

--- public/index.php
+++ public/index.php
@@ -31,7 +31,7 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
 
 // Load our paths config file
 // This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . '../exampleproject.dev/moneyep_files/app/Config/Paths.php';
+require FCPATH . '../../moneyep_files/app/Config/Paths.php';
 // ^^^ Change this line if you move your application folder
 
 $paths = new Config\Paths();
